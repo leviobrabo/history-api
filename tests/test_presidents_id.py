8f93618f9ab7477daf4fa6aff00536564cb9b18f
leviobrabo <carlosjunior20313@gmail.com>
2023-12-16 00:29:21 -0300

    Adding test for error possibilities and results for presidents by id

---
+++ tests/test_presidents_id.py
@@ -0,0 +1,66 @@
+from pytest import mark, raises
+from history_api.presidents_id import get_president_by_id, LANGUAGES
+from pathlib import Path
+import json
+import os
+
+
+def test_should_work_with_capitalized_lang():
+    lang = 'EN'
+    id = '5'
+    result = get_president_by_id(lang, id)
+    assert result
+
+
+def test_should_return_an_error_saying_that_the_lang_does_not_exist():
+    lang = 'KS'
+    id = '2'
+    expected_error_message = f"Unsupported language: {lang.lower()}"
+
+    with raises(KeyError) as error:
+        get_president_by_id(lang, id)
+
+    assert expected_error_message.lower() in str(error.value).lower()
+
+
+def test_get_president_id_less_than_1():
+    lang = 'en'
+    invalid_id = '0'
+    with raises(KeyError) as error:
+        get_president_by_id(lang, invalid_id)
+    assert 'Provided president ID does not exist' in str(error.value)
+
+
+def test_get_president_id_greater_than_652():
+    lang = 'en'
+    invalid_id = '653'
+    with raises(KeyError) as error:
+        get_president_by_id(lang, invalid_id)
+    assert 'Provided president ID does not exist' in str(error.value)
+
+
+def test_json_files_have_expected_keys():
+    expected_keys = [str(key) for key in range(1, 653)]
+
+    data_dir = Path(__file__).resolve().parent / '..' / \
+        'history_api' / 'data' / 'presidents'
+
+    missing_keys = []
+
+    for lang in LANGUAGES.keys():
+        json_file = data_dir / f"presidents-{lang}.json"
+
+        if os.path.exists(json_file):
+            with open(json_file, 'r', encoding='utf-8') as file:
+                json_data = json.load(file)
+                missing_lang_keys = [
+                    key for key in expected_keys if key not in json_data]
+
+                if missing_lang_keys:
+                    missing_keys.append(
+                        f"Language: {lang}, Missing keys: {', '.join(missing_lang_keys)}")
+        else:
+            missing_keys.append(
+                f"JSON file {json_file} for language {lang} is missing")
+
+    assert not missing_keys, f"Missing keys: {', '.join(missing_keys)}"
